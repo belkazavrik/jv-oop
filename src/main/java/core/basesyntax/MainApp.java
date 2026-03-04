dd3eb28c79fa0814dd07e174f96ad9d0f822ce64
Evhenii Shylov <super.belkazavr@gmail.com>
2026-03-04 09:22:51 +0200

    fixed a few checkstyle problems

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,7 +3,6 @@ package core.basesyntax;
 public class MainApp {
     public static void main(String[] args) {
 
-
         Machine[] machineList = new Machine[3];
 
         machineList[0] = new Truck();
